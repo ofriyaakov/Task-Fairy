14c2bdc7b9635bfed0ef847f2920ce69ecb56715
inbar112 <inbaradany@gmail.com>
2025-03-31 13:11:31 +0300

    created server folder with login login connected to client

---
+++ server/src/controllers/user.ts
@@ -0,0 +1,37 @@
+import userModel, { IUser } from "../models/user";
+
+export const getAllUsers = () => userModel.find();
+
+export const getUserById = (id) => {
+  const user = userModel.findById(id);
+  if (!user) throw new Error("User not found");
+  return user;
+};
+
+export const getUserByEmail = (email: IUser["email"]) => {
+  const user = userModel.findOne({ email });
+  if (!user) throw new Error("User not found");
+  return user;
+};
+
+export const addNewUser = (user: IUser) => userModel.create(user);
+
+export const updateUserById = (
+  id,
+  {
+    name,
+    gender,
+    height,
+    weight,
+    fitLevel,
+    image,
+  }: Omit<IUser, "password" | "email" | "tokens">
+) =>
+  userModel.findByIdAndUpdate(
+    id,
+    { name, gender, fitLevel, weight, height, image },
+    { new: true }
+  );
+
+export const updateUserTokenById = (id, newRefreshToken) =>
+  userModel.findByIdAndUpdate(id, { tokens: newRefreshToken }, { new: true });
