795e8163e40b3372f061207b313511a7c60978d1
InbarAdany <85437672+InbarAdany@users.noreply.github.com>
2025-03-31 13:16:08 +0300

    Update axiosInstance port

--- client/src/axiosInstance.ts
+++ client/src/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3001/", // Replace with your Express server's base URL
+  baseURL: "http://localhost:5000/", // Replace with your Express server's base URL
   timeout: 10000, // Optional: Set a timeout for requests
   headers: {
     "Content-Type": "application/json", // Default content type
