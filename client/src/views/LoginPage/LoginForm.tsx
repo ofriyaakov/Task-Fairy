b9010eacb409979e368c7f99cd731951bfffa63a
twqDaniell <thewickedqueend@gmail.com>
2025-04-13 15:16:40 +0300

    fix image sizes

--- client/src/views/LoginPage/LoginForm.tsx
+++ client/src/views/LoginPage/LoginForm.tsx
@@ -38,7 +38,7 @@ const LoginForm: React.FC<LoginFormProps> = ({ onSubmit }) => {
 
   return (
     <Container maxWidth='xs' sx={{ pt: 4, textAlign: "center" }}>
-      <img src='/Logo.png' alt='Logo' />
+      <img src='/Logo.png' alt='Logo' width={350} height={270} />
 
       <Box component='form' onSubmit={handleSubmit}>
         {error && (
