093605d93884620c0e9a33bd7044d1384555a802
Ofri Yaakov <ofriyaakov@gmail.com>
2025-05-04 23:52:26 +0300

    add swap card component

---
+++ client/src/components/SwapRequestCard/index.tsx
@@ -0,0 +1,128 @@
+import React from "react";
+import { Grid, Box, Typography, Paper } from "@mui/material";
+import PersonIcon from "@mui/icons-material/Person";
+import CompareArrowsIcon from "@mui/icons-material/CompareArrows";
+
+interface SwapCardDetails {
+    employeeId: string;
+    employeeFirstName: string;
+    employeeLastName: string;
+    taskName: string;
+    taskStartTime: string;
+    taskEndTime: string;
+}
+
+interface SwapDetailsProps {
+    employeeWithTask: SwapCardDetails
+}
+
+interface SwapRequestCardProps {
+    leftDetails: SwapCardDetails;
+    rightDetails: SwapCardDetails;
+}
+
+const SwapDetails: React.FC<SwapDetailsProps> = ({ employeeWithTask }) => {
+
+    const taskDate = new Date(employeeWithTask.taskStartTime).toLocaleDateString('de-CH')
+    const taskStartTime = new Date(employeeWithTask.taskStartTime).toLocaleTimeString()
+    const taskEndTime = new Date(employeeWithTask.taskEndTime).toLocaleTimeString()
+
+    return (
+        <Box>
+            <Box sx={{ display: "flex", alignItems: "center", mb: 1 }}>
+                <PersonIcon sx={{ color: "#000", mr: 2 }} />
+                <Box sx={{ justifyItems: 'start' }}>
+                    <Typography sx={{ lineHeight: 1.2 }}>
+                        {employeeWithTask.employeeFirstName} {employeeWithTask.employeeLastName}
+                    </Typography>
+                    <Typography>
+                        {employeeWithTask.employeeId}
+                    </Typography>
+                    <Typography sx={{ fontWeight: 650 }}>
+                        {taskDate}
+                    </Typography>
+                    <Typography sx={{ fontWeight: 650 }}>
+                        {taskStartTime} - {taskEndTime}
+                    </Typography>
+                </Box>
+            </Box>
+        </Box>
+    );
+};
+
+const SwapRequestCard: React.FC<SwapRequestCardProps> = ({
+    leftDetails,
+    rightDetails
+}) => {
+
+    const isSameTask = leftDetails.taskName === rightDetails.taskName
+
+    return (
+        <Paper
+            elevation={0}
+            sx={{
+                p: 3,
+                borderRadius: 4,
+                mb: 2,
+                width: "48%",
+                border: "1px solid rgb(229 229 229)",
+                height: '129px'
+            }}
+        >
+            {isSameTask ?
+                <Typography
+                    variant="h6"
+                    sx={{
+                        textAlign: "center",
+                        fontWeight: "bold",
+                        mb: 3
+                    }}
+                >
+                    {leftDetails.taskName}
+                </Typography> :
+                <Grid
+                    sx={{
+                        display: "flex",
+                        justifyContent: "space-around",
+                    }}>
+                    <Typography
+                        variant="h6"
+                        sx={{
+                            textAlign: "center",
+                            fontWeight: "bold",
+                            mb: 3
+                        }}
+                    >
+                        {leftDetails.taskName}
+                    </Typography>
+                    <Typography
+                        variant="h6"
+                        sx={{
+                            textAlign: "center",
+                            fontWeight: "bold",
+                            mb: 3
+                        }}
+                    >
+                        {rightDetails.taskName}
+                    </Typography>
+                </Grid>
+
+            }
+            <Grid container alignItems="center">
+                <Grid item xs={5}>
+                    <SwapDetails employeeWithTask={leftDetails} />
+                </Grid>
+
+                <Grid item xs={2} sx={{ textAlign: "center", mb: '2rem' }}>
+                    <CompareArrowsIcon sx={{ fontSize: 32 }} />
+                </Grid>
+
+                <Grid item xs={5}>
+                    <SwapDetails employeeWithTask={rightDetails} />
+                </Grid>
+            </Grid>
+        </Paper>
+    );
+};
+
+export default SwapRequestCard;
--- client/src/views/ManagerSwapsPage/ManagerSwapsPage.tsx
+++ client/src/views/ManagerSwapsPage/ManagerSwapsPage.tsx
@@ -1,6 +1,30 @@
+import SwapRequestCard from "../../components/SwapRequestCard";
+
 const ManagerSwapsPage: React.FC = () => {
-    return <div className='App'>Manager Swaps</div>;
-  };
-  
-  export default ManagerSwapsPage;
-  
+
+  const leftDetails = {
+    employeeId: 's2126075',
+    employeeFirstName: 'ofri',
+    employeeLastName: 'yaakov',
+    taskName: 'Contractors Supervision',
+    taskStartTime: '2025-04-29 06:07:15.046895',
+    taskEndTime: '2025-04-27 19:07:35.951148',
+  }
+  const rightDetails = {
+    employeeId: 's1234567',
+    employeeFirstName: 'yuval',
+    employeeLastName: 'hikry',
+    taskName: 'fill buckets',
+    taskStartTime: '2025-05-27 19:07:42.171136',
+    taskEndTime: '2025-05-27 19:58:42.171136',
+  }
+
+  return <div className='App'>
+    <div>
+      Manager Swaps
+    </div>
+    <SwapRequestCard leftDetails={leftDetails} rightDetails={rightDetails} />
+  </div>;
+};
+
+export default ManagerSwapsPage;
