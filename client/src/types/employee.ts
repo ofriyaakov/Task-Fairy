55148c8c82aff7b25b75ba523b5e7dceb5f00764
twqDaniell <thewickedqueend@gmail.com>
2025-05-17 12:48:44 +0300

    adding new employees from excel

--- client/src/types/employee.ts
+++ client/src/types/employee.ts
@@ -1,11 +1,21 @@
 export interface employeeDatailsCard {
-    first_name: string;
-    last_name: string;
-    user_id: string;
-    company_name: string;
-    city: string
-    balance_points: number;
-    gender: string;
-    score: number;
-    group_name: string;
+  first_name: string;
+  last_name: string;
+  user_id: string;
+  company_name: string;
+  city: string;
+  balance_points: number;
+  gender: string;
+  score: number;
+  group_name: string;
+}
+
+export interface newEmployee {
+  user_id: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  gender: string;
+  group_name: string;
+  phone_number: string;
 }
