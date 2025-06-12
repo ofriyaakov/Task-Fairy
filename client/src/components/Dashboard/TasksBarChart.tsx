a431ad93e0bf5fd1c31949a63ef8cce517ae2772
twqDaniell <thewickedqueend@gmail.com>
2025-06-12 13:27:01 +0300

    fix show actual month in dashboard

--- client/src/components/Dashboard/TasksBarChart.tsx
+++ client/src/components/Dashboard/TasksBarChart.tsx
@@ -20,20 +20,25 @@ const TasksBarChart: React.FC<TasksBarChartProps> = ({
   TasksBarChartValues,
 }) => {
   return (
-    <div className='bg-white rounded-xl shadow-md p-4'>
-      <div className='flex justify-between items-center mb-4'>
-        <h2 className='text-lg font-semibold'>Tasks amount</h2>
-        <span className='text-sm text-gray-500'>Jan 2025</span>
+    <div className="bg-white rounded-xl shadow-md p-4">
+      <div className="flex justify-between items-center mb-4">
+        <h2 className="text-lg font-semibold">Task amount</h2>
+        <span className="text-sm text-gray-500">
+          {new Date().toLocaleString("en-US", {
+            month: "short",
+            year: "numeric",
+          })}
+        </span>
       </div>
-      <ResponsiveContainer width='100%' height={250}>
+      <ResponsiveContainer width="100%" height={250}>
         <BarChart data={TasksBarChartValues}>
-          <CartesianGrid strokeDasharray='3 3' vertical={false} />
-          <XAxis dataKey='group' />
+          <CartesianGrid strokeDasharray="3 3" vertical={false} />
+          <XAxis dataKey="group" />
           <YAxis domain={[0, 100]} tickFormatter={(value) => `${value}%`} />
           <Tooltip formatter={(value) => `${value}%`} />
           <Bar
-            dataKey='percentage'
-            fill='#4db4f6'
+            dataKey="percentage"
+            fill="#4db4f6"
             barSize={40}
             radius={[4, 4, 0, 0]}
           />
