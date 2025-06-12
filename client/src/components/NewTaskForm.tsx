937a64485e4507b6afd5b879f121bedbaf31d83d
Yuval Hikry <hashlama53@bsmch.net>
2025-06-12 19:30:53 +0300

    limit

--- client/src/components/NewTaskForm.tsx
+++ client/src/components/NewTaskForm.tsx
@@ -467,6 +467,7 @@ const NewTaskForm: React.FC<NewTaskFormProps> = ({ initialData }) => {
                       onChange={(newValue) =>
                         handleChange("endTime", newValue?.toDate())
                       }
+                      minTime={dayjs(formData.startTime)} 
                       sx={{ bgcolor: "white" }}
                     />
                   </Box>
